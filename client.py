169c4f62d2057428fbe6d1db533354f5479c910b
zuoxiaoliang <zuoxiaoliang@wolongdata.com>
2017-03-21 08:34:46 -0700

    细化调度

---
+++ client.py
@@ -0,0 +1,17 @@
+
+#coding:utf-8
+import socket
+#链接服务端ip和端口
+ip_port = ('127.0.0.1',8888)
+#生成一个句柄
+sk = socket.socket()
+#请求连接服务端
+sk.connect(ip_port)
+#发送数据
+sk.sendall(bytes('yaoyao','utf8'))
+#接受数据
+#server_reply = sk.recv(1024)
+#打印接受的数据
+#print (str(server_reply,'utf8'))
+#关闭连接
+sk.close()
--- test.py
+++ test.py
@@ -8,27 +8,44 @@ import socket
 
 class Net:
 
-    def __init__(self, loop):
+    def __init__(self):
         self._selector = selectors.DefaultSelector()
+        self._ready = collections.deque()
         sock = socket.socket()
-        sock.bind(('localhost', 1234))
+        sock.bind(('localhost', 8888))
         sock.listen(100)
         sock.setblocking(False)
-        self.loop = loop
+        # self.loop = loop
         self._selector.register(sock, selectors.EVENT_READ, self.accept)
         # self.loop.call_soon()
+        # self.call_soon()
+
+
 
     def accept(self, sock, mask):
+        fut = Future()
+        # conn, addr = sock.accept()  # Should be ready
+        # print('accepted', conn, 'from', addr)
+        # conn.setblocking(False)
+        # self._selector.register(conn, selectors.EVENT_READ, self.each_connect)
+
+        return fut
+
+    def accept2(self, sock):
+        fut = Future()
         conn, addr = sock.accept()  # Should be ready
         print('accepted', conn, 'from', addr)
         conn.setblocking(False)
         self._selector.register(conn, selectors.EVENT_READ, self.each_connect)
 
-
     def each_connect(self, sock, mask):
-        fut = Future()
-        self.recv(fut, sock, 20)
-        return fut
+        #fut = Future()
+        #.recv(fut, sock, 20)
+        #return fut
+        data = sock.recv(20)
+        print("data", data)
+        sock.close()
+        self._selector.unregister(sock)
 
 
     def recv(self, fut, sock, n):
@@ -38,6 +55,7 @@ class Net:
         try:
             data = sock.recv(n)
         except (BlockingIOError, InterruptedError):
+            print("ggg")
             self.add_reader(fd, self.recv, fut, sock, n)
         except Exception as exc:
             fut.set_exception(exc)
@@ -50,11 +68,17 @@ class Net:
             key = self._selector.get_key(fd)
         except KeyError:
             self._selector.register(fd, selectors.EVENT_READ,
-                                    (handle, None))
+                                    handle)
         else:
-            mask, (reader, writer) = key.events, key.data
-            self._selector.modify(fd, mask | selectors.EVENT_READ,
-                                  (handle, writer))
+            mask, reader= key.events, key.data
+            self._selector.modify(fd, selectors.EVENT_READ, handle)
+
+    def add_callback(self, handle):
+        self._ready.append(handle)
+
+    def run_once(self):
+
+
 
 import collections
 
@@ -63,9 +87,24 @@ class Loop:
         self._ready = collections.deque()
 
     def run_for_ever(self):
-        p
+        pass
+
 
 def server_start():
     loop = Loop()
-    serv = Net(loop)
-    loop.
+    serv = Net()
+    index = 0
+    while index >= 0:
+        index += 1
+        events = serv._selector.select(2)
+        for key, mask in events:
+            # print("kkk")
+            # print(mask, key.fileobj)
+            fileobj, reader = key.fileobj, key.data
+            # callback = key.data
+            # callback(key.fileobj, mask)
+            serv.add_callback(reader)
+
+
+if __name__ == '__main__':
+    server_start()
